a00fad79eedc7bb041df4bf290d489915898847d
lumeCraftors <lumecraftor@gmail.com>
2025-03-22 12:29:44 +0800

    Create gameslist.lua

---
+++ gameslist.lua
@@ -0,0 +1,5 @@
+return {
+    [124456677886] = "https://pastebin.com/raw/YpsKTyGk", -- Example Game 1
+    [273738328393] = "https://raw.githubusercontent.com/example/script.lua", -- Example Game 2
+    -- Add more game IDs with script links
+}
