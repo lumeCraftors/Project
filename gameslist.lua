5117016b5dfdbdb50c0521388fddf5b6d418277c
lumeCraftors <lumecraftor@gmail.com>
2025-03-24 02:12:11 +0800

    Update gameslist.lua

--- gameslist.lua
+++ gameslist.lua
@@ -1,5 +1,17 @@
-return {
-    [124456677886] = "https://pastebin.com/raw/YpsKTyGk", -- Example Game 1
-    [273738328393] = "https://raw.githubusercontent.com/example/script.lua", -- Example Game 2
-    -- Add more game IDs with script links
+local gameScripts = {
+    [1234567890] = "https://raw.githubusercontent.com/YOUR_REPO/Script1.lua", -- Replace with actual game IDs & scripts
+    [9876543210] = "https://raw.githubusercontent.com/YOUR_REPO/Script2.lua"
 }
+
+local placeId = game.PlaceId
+
+if gameScripts[placeId] then
+    local scriptURL = gameScripts[placeId]
+    loadstring(game:HttpGet(scriptURL, true))()
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "❌ Unsupported Game",
+        Text = "This game is not supported.",
+        Duration = 5
+    })
+end
